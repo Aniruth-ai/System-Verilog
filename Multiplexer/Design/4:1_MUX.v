52118b990abbdcbd379ab6212e0dc2b7094cd6a1
Aniruth-ai <62967917+Aniruth-ai@users.noreply.github.com>
2020-08-05 14:24:44 -0500

    Update 4:1_MUX.v

--- Multiplexer/Design/4:1_MUX.v
+++ Multiplexer/Design/4:1_MUX.v
@@ -1,12 +1,14 @@
 module 4:1_MUX(clk, in0, in1, in2, in3, sel, out, reset);
   
   input clk;
+  input reset;
+  
   input [3:0] in0;
   input [3:0] in1;
   input [3:0] in2;
   input [3:0] in3;
-  input reset;
-  input [1:0]sel;
+  input [1:0] sel;
+  
   output reg [3:0] out;
   
   parameter S0 = 2'b00;
@@ -16,20 +18,16 @@ module 4:1_MUX(clk, in0, in1, in2, in3, sel, out, reset);
   
   always @(posedge clk) begin
     
-    if(reset)
-      
+    if(reset)      
       out <= 0;
     
     else begin
-      
-      case(sel) 
+     
+      case(sel)
 
         (S0): out <= in0;
-
         (S1): out <= in1;
-
         (S2): out <= in2;
-
         (S3): out <= in3;
 
         default: begin
